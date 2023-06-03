7c2c09d00731604289e1c4e6b64df7d435158626
stastnan <nik.stastna@gmail.com>
2023-06-03 19:28:12 +0200

    Added Web components project

---
+++ script.js
@@ -0,0 +1,70 @@
+const appSidebarButtonTemplate = document.createElement("template");
+appSidebarButtonTemplate.innerHTML = `
+    <style>
+
+    body {
+        margin-left: 0;
+        padding-left: 0;
+    }
+   button {
+        padding: 1rem 1rem;
+
+        height: 2.7em;
+        width: 17rem;
+        background: none;
+        border: none;
+        display: flex;
+        align-items: center;
+        border-radius: 0px 50px 50px 0px;
+        font-size: 1.2rem;
+        display: flex;
+        gap: 1rem;
+        padding-left: 2.3rem;
+        cursor: pointer;
+        }
+    
+    button:hover {
+        background-color: rgb(220,220,220);
+    }    
+    #unreadCount {
+        position: relative;
+        right: 0;
+    }
+
+    .button__text_position-right {
+        display: inline-block;
+        font-size: 0.9rem;
+        color: rgb(106, 105, 105);
+        margin-left: 100px;
+    }
+
+    </style>
+
+
+    <button>
+        <slot class="button__icon"></slot>
+        <span id="text"></span>
+        <span id="unreadCount" class="button__text_position-right"></span>
+    </button>
+
+`
+class appSidebarButton extends HTMLElement {
+    constructor() {
+        super();
+    
+    this.attachShadow({ mode:"open" })
+    }
+
+    connectedCallback () {
+        this.shadowRoot.append(appSidebarButtonTemplate.content.cloneNode(true))
+        const text = this.getAttribute("text");
+        const unreadCount = this.getAttribute("unreadCount");
+
+        this.shadowRoot.querySelector("#text").innerText = text;
+        this.shadowRoot.querySelector("#unreadCount").innerText = unreadCount;
+    }
+   
+}
+
+window.customElements.define("app-sidebar-button", appSidebarButton)
+
