db1c194e2e613e92fe9ad29e2d8a20da78cd2d4e
stastnan <nik.stastna@gmail.com>
2023-09-01 18:44:45 +0100

    Added styles for selected button + small fixes

--- script.js
+++ script.js
@@ -47,24 +47,28 @@ appSidebarButtonTemplate.innerHTML = `
         <span id="unreadCount" class="button__text_position-right"></span>
     </button>
 
-`
-class appSidebarButton extends HTMLElement {
-    constructor() {
-        super();
-    
-    this.attachShadow({ mode:"open" })
-    }
+`;
+class AppSidebarButton extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
 
-    connectedCallback () {
-        this.shadowRoot.append(appSidebarButtonTemplate.content.cloneNode(true))
-        const text = this.getAttribute("text");
-        const unreadCount = this.getAttribute("unreadCount");
+  connectedCallback() {
+    this.shadowRoot.append(appSidebarButtonTemplate.content.cloneNode(true));
+    const text = this.getAttribute("text");
+    const unreadCount = this.getAttribute("unreadCount");
+    const selected = this.getAttribute("selected");
 
-        this.shadowRoot.querySelector("#text").innerText = text;
-        this.shadowRoot.querySelector("#unreadCount").innerText = unreadCount;
+    this.shadowRoot.querySelector("#text").innerText = text;
+    this.shadowRoot.querySelector("#unreadCount").innerText = unreadCount;
+
+    if (selected === "true") {
+      const button = this.shadowRoot.querySelector("button");
+      button.style.backgroundColor = "#D3E2FC";
+      button.style.fontWeight = "bold";
     }
-   
+  }
 }
 
-window.customElements.define("app-sidebar-button", appSidebarButton)
-
+window.customElements.define("app-sidebar-button", AppSidebarButton);
